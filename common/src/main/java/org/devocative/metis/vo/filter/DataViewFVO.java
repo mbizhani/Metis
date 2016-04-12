4d7261160efb9b4ba61921e439869211a8f2c38a
Mehdi Bizhani <mbizhani@gmail.com>
2016-04-12 21:48:38 +0430

    Adding DataViewListDPage

---
+++ common/src/main/java/org/devocative/metis/vo/filter/DataViewFVO.java
@@ -0,0 +1,85 @@
+package org.devocative.metis.vo.filter;
+
+import org.devocative.adroit.vo.RangeVO;
+import org.devocative.demeter.entity.User;
+import org.devocative.demeter.iservice.persistor.Filterer;
+import org.devocative.metis.entity.data.DataSource;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+@Filterer
+public class DataViewFVO implements Serializable {
+	private String name;
+
+	private String title;
+
+	private List<DataSource> dataSource;
+
+	private RangeVO<Date> creationDate;
+
+	private List<User> creatorUser;
+
+	private RangeVO<Date> modificationDate;
+
+	private List<User> modifierUser;
+
+	// ------------------- ACCESSORS
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public List<DataSource> getDataSource() {
+		return dataSource;
+	}
+
+	public void setDataSource(List<DataSource> dataSource) {
+		this.dataSource = dataSource;
+	}
+
+	public RangeVO<Date> getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(RangeVO<Date> creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public List<User> getCreatorUser() {
+		return creatorUser;
+	}
+
+	public void setCreatorUser(List<User> creatorUser) {
+		this.creatorUser = creatorUser;
+	}
+
+	public RangeVO<Date> getModificationDate() {
+		return modificationDate;
+	}
+
+	public void setModificationDate(RangeVO<Date> modificationDate) {
+		this.modificationDate = modificationDate;
+	}
+
+	public List<User> getModifierUser() {
+		return modifierUser;
+	}
+
+	public void setModifierUser(List<User> modifierUser) {
+		this.modifierUser = modifierUser;
+	}
+}
---
+++ web/src/main/java/org/devocative/metis/web/dPage/DataViewListDPage.java
@@ -0,0 +1,118 @@
+package org.devocative.metis.web.dPage;
+
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.model.ResourceModel;
+import org.devocative.demeter.entity.User;
+import org.devocative.demeter.iservice.IUserService;
+import org.devocative.demeter.web.DPage;
+import org.devocative.demeter.web.component.DAjaxButton;
+import org.devocative.demeter.web.component.grid.ORESTLinkColumn;
+import org.devocative.metis.entity.data.DataView;
+import org.devocative.metis.iservice.IDataSourceService;
+import org.devocative.metis.iservice.IDataViewService;
+import org.devocative.metis.vo.filter.DataViewFVO;
+import org.devocative.metis.web.MetisIcon;
+import org.devocative.metis.web.dPage.data.form.DataViewFormDPage;
+import org.devocative.wickomp.WModel;
+import org.devocative.wickomp.form.WDateRangeInput;
+import org.devocative.wickomp.form.WSelectionInput;
+import org.devocative.wickomp.form.WTextInput;
+import org.devocative.wickomp.formatter.ODateFormatter;
+import org.devocative.wickomp.grid.IGridDataSource;
+import org.devocative.wickomp.grid.OGrid;
+import org.devocative.wickomp.grid.WDataGrid;
+import org.devocative.wickomp.grid.WSortField;
+import org.devocative.wickomp.grid.column.OColumnList;
+import org.devocative.wickomp.grid.column.OPropertyColumn;
+import org.devocative.wickomp.html.WFloatTable;
+import org.devocative.wickomp.opt.OSize;
+
+import javax.inject.Inject;
+import java.util.List;
+
+public class DataViewListDPage extends DPage implements IGridDataSource<DataView> {
+	private DataViewFVO filter = new DataViewFVO();
+
+	private WDataGrid<DataView> grid;
+
+	@Inject
+	private IDataViewService dataViewService;
+
+	@Inject
+	private IDataSourceService dataSourceService;
+
+	@Inject
+	private IUserService userService;
+
+	public DataViewListDPage(String id, List<String> params) {
+		super(id, params);
+
+		List<User> users = userService.list();
+
+		Form<DataViewFVO> form = new Form<>("form", new CompoundPropertyModel<>(filter));
+		WFloatTable floatTable = new WFloatTable("floatTable");
+		floatTable.setEqualWidth(true);
+		floatTable.add(new WTextInput("name").setLabel(new ResourceModel("DataView.name")));
+		floatTable.add(new WTextInput("title").setLabel(new ResourceModel("DataView.title")));
+		floatTable.add(new WSelectionInput("dataSource", dataSourceService.list(), true)
+			.setLabel(new ResourceModel("DataView.dataSource")));
+
+		floatTable.add(new WDateRangeInput("creationDate").setLabel(new ResourceModel("entity.creationDate")));
+		floatTable.add(new WSelectionInput("creatorUser", users, true).setLabel(new ResourceModel("entity.creatorUser")));
+		floatTable.add(new WDateRangeInput("modificationDate").setLabel(new ResourceModel("entity.modificationDate")));
+		floatTable.add(new WSelectionInput("modifierUser", users, true).setLabel(new ResourceModel("entity.modifierUser")));
+		form.add(floatTable);
+		form.add(new DAjaxButton("search", new ResourceModel("label.search"), MetisIcon.SEARCH) {
+			@Override
+			protected void onSubmit(AjaxRequestTarget target) {
+				grid.setEnabled(true);
+				grid.loadData(target);
+			}
+		});
+		add(form);
+
+		OColumnList<DataView> columnList = new OColumnList<>();
+		columnList.add(new OPropertyColumn<DataView>(new ResourceModel("DataView.name"), "name"));
+		columnList.add(new OPropertyColumn<DataView>(new ResourceModel("DataView.title"), "title"));
+		columnList.add(new OPropertyColumn<DataView>(new ResourceModel("DataView.dataSource"), "dataSource"));
+
+		columnList.add(new OPropertyColumn<DataView>(new ResourceModel("entity.creationDate"), "creationDate")
+			.setFormatter(ODateFormatter.getDateTimeByUserPreference()));
+		columnList.add(new OPropertyColumn<DataView>(new ResourceModel("entity.creatorUser"), "creatorUser"));
+		columnList.add(new OPropertyColumn<DataView>(new ResourceModel("entity.modificationDate"), "modificationDate")
+			.setFormatter(ODateFormatter.getDateTimeByUserPreference()));
+		columnList.add(new OPropertyColumn<DataView>(new ResourceModel("entity.modifierUser"), "modifierUser"));
+
+		columnList.add(new ORESTLinkColumn<DataView>(new Model<String>(), DataViewFormDPage.class, "name", MetisIcon.EDIT));
+		//columnList.add(new ORESTLinkColumn<DataView>(new Model<String>(), DataSourceExecutor.class, "name", MetisIcon.EXECUTE));
+
+		OGrid<DataView> oGrid = new OGrid<>();
+		oGrid
+			.setColumns(columnList)
+			.setMultiSort(false)
+			.setHeight(OSize.fixed(350))
+			.setWidth(OSize.percent(100));
+
+		add(grid = new WDataGrid<>("grid", oGrid, this));
+		grid.setEnabled(false);
+
+	}
+
+	public List<DataView> list(long pageIndex, long pageSize, List<WSortField> sortFields) {
+		return dataViewService.search(filter, pageIndex, pageSize);
+	}
+
+	@Override
+	public long count() {
+		return dataViewService.count(filter);
+	}
+
+	@Override
+	public IModel<DataView> model(DataView object) {
+		return new WModel<>(object);
+	}
+}
