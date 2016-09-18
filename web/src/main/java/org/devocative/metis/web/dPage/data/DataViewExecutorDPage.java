b17ced6fcb39c0b17ae6c5125584ba1e5af032da
Mehdi Bizhani <mbizhani@gmail.com>
2016-09-18 21:12:24 +0430

    changed by Wickomp

--- web/src/main/java/org/devocative/metis/web/dPage/data/DataViewExecutorDPage.java
+++ web/src/main/java/org/devocative/metis/web/dPage/data/DataViewExecutorDPage.java
@@ -23,7 +23,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
-import java.io.Serializable;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -125,14 +124,6 @@ public class DataViewExecutorDPage extends DPage {
 			searchBut.getMarkupId(), mainGrid.getGridHtmlId());
 		WebUtil.writeJQueryCall(scriptForLoading, false);
 
-		final List<Serializable> errors = WebUtil.collectAs(this, true);
-
-		if (errors.size() > 0) {
-			String st = WMessager.getScript(
-				getString("label.error", null, "Error"),
-				WMessager.getHtml(errors));
-
-			WebUtil.writeJQueryCall(st, true);
-		}
+		WMessager.writeErrorsInAfterRender(this);
 	}
 }
