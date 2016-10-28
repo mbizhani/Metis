b8545b438bea44e3e8f9e8c2d862d51a62ba6daa
Mehdi Bizhani <mbizhani@gmail.com>
2016-10-28 14:09:39 +0330

    using IconFont for MetisIcon

--- web/src/main/java/org/devocative/metis/web/MetisIcon.java
+++ web/src/main/java/org/devocative/metis/web/MetisIcon.java
@@ -2,10 +2,10 @@ package org.devocative.metis.web;
 
 import org.apache.wicket.model.ResourceModel;
 import org.devocative.demeter.web.DemeterIcon;
-import org.devocative.wickomp.html.HTMLBase;
 import org.devocative.wickomp.html.icon.FontAwesome;
+import org.devocative.wickomp.html.icon.IconFont;
 
 public class MetisIcon extends DemeterIcon {
-	public static final HTMLBase CHECK_CONNECTION = new FontAwesome("plug", new ResourceModel("DBConnection.testQuery"));
-	public static final HTMLBase DEFAULT_CONNECTION = new FontAwesome("dot-circle-o", new ResourceModel("DBConnection.user.default"));
+	public static final IconFont CHECK_CONNECTION = new FontAwesome("plug", new ResourceModel("DBConnection.testQuery"));
+	public static final IconFont DEFAULT_CONNECTION = new FontAwesome("dot-circle-o", new ResourceModel("DBConnection.user.default"));
 }
