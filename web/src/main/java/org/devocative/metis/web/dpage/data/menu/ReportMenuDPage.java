9fd0782fef0bcf1190755773eba39dcc91c454ea
Mehdi Bizhani <mbizhani@gmail.com>
2017-01-02 21:13:28 +0330

    adding some style for report menu

--- web/src/main/java/org/devocative/metis/web/dpage/data/menu/ReportMenuDPage.java
+++ web/src/main/java/org/devocative/metis/web/dpage/data/menu/ReportMenuDPage.java
@@ -1,10 +1,11 @@
 package org.devocative.metis.web.dpage.data.menu;
 
+import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.model.ResourceModel;
 import org.devocative.demeter.web.DPage;
 import org.devocative.metis.entity.data.DataGroup;
 import org.devocative.metis.entity.data.Report;
@@ -29,7 +30,8 @@ public class ReportMenuDPage extends DPage {
 		super(id, params);
 
 		tabPanel = new WTabbedPanel("tabPanel");
-		tabPanel.addTab(new ReportMenuPanel(tabPanel.getTabContentId()), new Model<>("Reports"));
+		tabPanel.addTab(new ReportMenuPanel(tabPanel.getTabContentId()),
+			new ResourceModel("Report.menu.tab", "Reports List"));
 
 		add(tabPanel);
 
@@ -39,7 +41,7 @@ public class ReportMenuDPage extends DPage {
 
 	// ------------------------------
 
-	public class ReportMenuPanel extends WPanel {
+	private class ReportMenuPanel extends WPanel {
 		private static final long serialVersionUID = -9012565863392514416L;
 
 		@Inject
@@ -62,7 +64,8 @@ public class ReportMenuDPage extends DPage {
 				protected void populateItem(ListItem<DataGroup> item) {
 					DataGroup group = item.getModelObject();
 
-					item.add(new Label("groupTitle", group.getName()));
+					//item.add(new Label("groupTitle", group.getName()));
+					item.add(new AttributeModifier("title", group.getName()));
 
 					item.add(new ListView<Report>("report", listPerGroup.get(group)) {
 						private static final long serialVersionUID = 7979832690560075180L;
@@ -71,9 +74,7 @@ public class ReportMenuDPage extends DPage {
 						protected void populateItem(ListItem<Report> item) {
 							final Report report = item.getModelObject();
 
-							item.add(new Label("reportTitle", report.getTitle()));
-
-							item.add(new WAjaxLink("run") {
+							WAjaxLink run = new WAjaxLink("run", new Model<>(report.getTitle())) {
 								private static final long serialVersionUID = -2507993467733702720L;
 
 								@Override
@@ -83,7 +84,9 @@ public class ReportMenuDPage extends DPage {
 											report),
 										new OTab(new Model<>(report.getTitle()), true));
 								}
-							});
+							};
+							//run.add(new AttributeModifier("title", report.getTitle()));
+							item.add(run);
 						}
 					});
 				}
