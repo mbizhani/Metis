87c97c3080cb7eea02d7a7b1c4b416cafce7cdd1
Mehdi Bizhani <mbizhani@gmail.com>
2017-10-21 20:48:56 +0330

    * Extending the test artifact of Demeter and simple test for Metis

---
+++ web/src/test/java/org/devocative/metis/test/TestMetis.java
@@ -0,0 +1,12 @@
+package org.devocative.metis.test;
+
+import org.devocative.demeter.web.test.TestDemeter;
+
+public class TestMetis extends TestDemeter {
+	{
+		NO_OF_DB_DIFF_FILES =
+			2 // DEMETER
+				+
+				2; //METIS
+	}
+}
---
+++ web/src/test/java/org/devocative/metis/test/TestMetisByOracle.java
@@ -0,0 +1,9 @@
+package org.devocative.metis.test;
+
+import org.devocative.demeter.web.test.TestDemeterByOracle;
+
+public class TestMetisByOracle extends TestDemeterByOracle {
+	{
+		TEST_DEMETER_CLASS = TestMetis.class;
+	}
+}
