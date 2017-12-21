82b6134ffa195f921ce63a19261bab88176451d5
Mehdi Bizhani <mbizhani@gmail.com>
2017-12-21 16:05:15 +0330

    minor change

--- web/src/test/java/org/devocative/metis/test/TestMetis.java
+++ web/src/test/java/org/devocative/metis/test/TestMetis.java
@@ -3,10 +3,4 @@ package org.devocative.metis.test;
 import org.devocative.demeter.web.test.TestDemeter;
 
 public class TestMetis extends TestDemeter {
-	{
-		NO_OF_DB_DIFF_FILES =
-			2 // DEMETER
-				+
-				2; //METIS
-	}
 }
