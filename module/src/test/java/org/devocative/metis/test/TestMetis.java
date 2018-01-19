b9f6ef85e9f0e4f8b1772e7b232d4115aa4d0296
Mehdi Bizhani <mbizhani@gmail.com>
2018-01-19 21:23:56 +0330

    ** Refactor based on Demeter refactor of dependencies and auto module find

--- module/src/test/java/org/devocative/metis/test/TestMetis.java
+++ module/src/test/java/org/devocative/metis/test/TestMetis.java
@@ -1,6 +1,6 @@
 package org.devocative.metis.test;
 
-import org.devocative.demeter.web.test.TestDemeter;
+import org.devocative.demeter.test.TestDemeter;
 
 public class TestMetis extends TestDemeter {
 }
--- module/src/test/java/org/devocative/metis/test/TestMetisByOracle.java
+++ module/src/test/java/org/devocative/metis/test/TestMetisByOracle.java
@@ -1,6 +1,6 @@
 package org.devocative.metis.test;
 
-import org.devocative.demeter.web.test.TestDemeterByOracle;
+import org.devocative.demeter.test.TestDemeterByOracle;
 
 public class TestMetisByOracle extends TestDemeterByOracle {
 	{
