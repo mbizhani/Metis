cf7f13611e20777077fb97c8bacf65d1fd6731f7
Mehdi Bizhani <mbizhani@gmail.com>
2018-03-09 21:59:48 +0330

    Oracle SQL file for Metis DModule

--- common/src/main/resources/sql/mts_hsqldb_1.0-1.sql
+++
@@ -1,9 +0,0 @@
-----------------
--- ALTER TABLES
-----------------
-
-ALTER TABLE a_t_mts_db_conn ADD c_custom_param_1 VARCHAR(255);
-ALTER TABLE t_mts_db_conn ADD c_custom_param_1 VARCHAR(255);
-
-ALTER TABLE t_mts_data_src ADD e_conn_selection INTEGER NOT NULL;
-ALTER TABLE a_t_mts_data_src ADD e_conn_selection INTEGER;
--- common/src/main/resources/sql/mts_oracle_1.0.sql
+++ common/src/main/resources/sql/mts_oracle_1.0.sql
@@ -3,24 +3,24 @@
 -----------------------
 
 CREATE TABLE a_mt_mts_dataview_group (
-	r_num       NUMBER(10, 0) NOT NULL,
-	f_data_view NUMBER(19, 0) NOT NULL,
-	f_group     NUMBER(19, 0) NOT NULL,
+	r_num       NUMBER(10, 0)      NOT NULL,
+	f_data_view VARCHAR2(255 CHAR) NOT NULL,
+	f_group     VARCHAR2(255 CHAR) NOT NULL,
 	r_type      NUMBER(3, 0),
 	PRIMARY KEY (r_num, f_data_view, f_group)
 );
 
 CREATE TABLE a_mt_mts_report_group (
-	r_num    NUMBER(10, 0) NOT NULL,
-	f_report NUMBER(19, 0) NOT NULL,
-	f_group  NUMBER(19, 0) NOT NULL,
+	r_num    NUMBER(10, 0)      NOT NULL,
+	f_report VARCHAR2(255 CHAR) NOT NULL,
+	f_group  VARCHAR2(255 CHAR) NOT NULL,
 	r_type   NUMBER(3, 0),
 	PRIMARY KEY (r_num, f_report, f_group)
 );
 
 CREATE TABLE a_t_mts_cfg_lob (
-	id              NUMBER(19, 0) NOT NULL,
-	r_num           NUMBER(10, 0) NOT NULL,
+	id    VARCHAR2(255 CHAR) NOT NULL,
+	r_num NUMBER(10, 0)      NOT NULL,
 	r_type          NUMBER(3, 0),
 	f_modifier_user NUMBER(19, 0),
 	c_value         CLOB,
@@ -28,20 +28,22 @@ CREATE TABLE a_t_mts_cfg_lob (
 );
 
 CREATE TABLE a_t_mts_data_group (
-	id              NUMBER(19, 0) NOT NULL,
-	r_num           NUMBER(10, 0) NOT NULL,
+	id     VARCHAR2(255 CHAR) NOT NULL,
+	r_num  NUMBER(10, 0)      NOT NULL,
 	r_type          NUMBER(3, 0),
+	c_code VARCHAR2(255 CHAR),
 	f_modifier_user NUMBER(19, 0),
 	c_name          VARCHAR2(255 CHAR),
 	PRIMARY KEY (id, r_num)
 );
 
 CREATE TABLE a_t_mts_data_src (
-	id                       NUMBER(19, 0) NOT NULL,
-	r_num                    NUMBER(10, 0) NOT NULL,
+	id               VARCHAR2(255 CHAR) NOT NULL,
+	r_num            NUMBER(10, 0)      NOT NULL,
 	r_type                   NUMBER(3, 0),
-	f_config                 NUMBER(19, 0),
+	f_config         VARCHAR2(255 CHAR),
 	f_connection             NUMBER(19, 0),
+	e_conn_selection NUMBER(10, 0),
 	c_key_field              VARCHAR2(255 CHAR),
 	f_modifier_user          NUMBER(19, 0),
 	c_name                   VARCHAR2(255 CHAR),
@@ -52,25 +54,22 @@ CREATE TABLE a_t_mts_data_src (
 );
 
 CREATE TABLE a_t_mts_data_src_rel (
-	id                NUMBER(19, 0) NOT NULL,
-	r_num             NUMBER(10, 0) NOT NULL,
-	r_type            NUMBER(3, 0),
-	b_deleted         NUMBER(1, 0),
-	f_modifier_user   NUMBER(19, 0),
-	f_src_datasrc     NUMBER(19, 0),
-	c_src_ptr_field   VARCHAR2(255 CHAR),
-	f_tgt_datasrc     NUMBER(19, 0),
-	c_tgt_key_field   VARCHAR2(255 CHAR),
-	c_tgt_title_field VARCHAR2(255 CHAR),
+	id              VARCHAR2(255 CHAR) NOT NULL,
+	r_num           NUMBER(10, 0)      NOT NULL,
+	r_type          NUMBER(3, 0),
+	f_modifier_user NUMBER(19, 0),
+	f_src_datasrc   VARCHAR2(255 CHAR),
+	c_src_ptr_field VARCHAR2(255 CHAR),
+	f_tgt_datasrc   VARCHAR2(255 CHAR),
 	PRIMARY KEY (id, r_num)
 );
 
 CREATE TABLE a_t_mts_data_view (
-	id              NUMBER(19, 0) NOT NULL,
-	r_num           NUMBER(10, 0) NOT NULL,
+	id         VARCHAR2(255 CHAR) NOT NULL,
+	r_num      NUMBER(10, 0)      NOT NULL,
 	r_type          NUMBER(3, 0),
-	f_config        NUMBER(19, 0),
-	f_data_src      NUMBER(19, 0),
+	f_config   VARCHAR2(255 CHAR),
+	f_data_src VARCHAR2(255 CHAR),
 	f_modifier_user NUMBER(19, 0),
 	c_name          VARCHAR2(255 CHAR),
 	c_title         VARCHAR2(255 CHAR),
@@ -78,27 +77,39 @@ CREATE TABLE a_t_mts_data_view (
 );
 
 CREATE TABLE a_t_mts_db_conn (
+	id               NUMBER(19, 0) NOT NULL,
+	r_num            NUMBER(10, 0) NOT NULL,
+	r_type           NUMBER(3, 0),
+	f_config         VARCHAR2(255 CHAR),
+	c_custom_param_1 VARCHAR2(255 CHAR),
+	c_driver         VARCHAR2(255 CHAR),
+	f_group          VARCHAR2(255 CHAR),
+	f_modifier_user  NUMBER(19, 0),
+	c_name           VARCHAR2(255 CHAR),
+	c_password       VARCHAR2(255 CHAR),
+	c_schema         VARCHAR2(255 CHAR),
+	c_test_query     VARCHAR2(255 CHAR),
+	c_url            VARCHAR2(255 CHAR),
+	c_username       VARCHAR2(255 CHAR),
+	PRIMARY KEY (id, r_num)
+);
+
+CREATE TABLE a_t_mts_db_conn_alias (
 	id              NUMBER(19, 0) NOT NULL,
 	r_num           NUMBER(10, 0) NOT NULL,
 	r_type          NUMBER(3, 0),
-	f_config        NUMBER(19, 0),
-	c_driver        VARCHAR2(255 CHAR),
-	f_group         NUMBER(19, 0),
+	f_connection NUMBER(19, 0),
+	e_mode       NUMBER(10, 0),
 	f_modifier_user NUMBER(19, 0),
 	c_name          VARCHAR2(255 CHAR),
-	c_password      VARCHAR2(255 CHAR),
-	c_schema        VARCHAR2(255 CHAR),
-	c_test_query    VARCHAR2(255 CHAR),
-	c_url           VARCHAR2(255 CHAR),
-	c_username      VARCHAR2(255 CHAR),
 	PRIMARY KEY (id, r_num)
 );
 
 CREATE TABLE a_t_mts_db_conn_grp (
-	id              NUMBER(19, 0) NOT NULL,
-	r_num           NUMBER(10, 0) NOT NULL,
+	id       VARCHAR2(255 CHAR) NOT NULL,
+	r_num    NUMBER(10, 0)      NOT NULL,
 	r_type          NUMBER(3, 0),
-	f_config        NUMBER(19, 0),
+	f_config VARCHAR2(255 CHAR),
 	c_driver        VARCHAR2(255 CHAR),
 	f_modifier_user NUMBER(19, 0),
 	c_name          VARCHAR2(255 CHAR),
@@ -108,11 +119,11 @@ CREATE TABLE a_t_mts_db_conn_grp (
 );
 
 CREATE TABLE a_t_mts_report (
-	id              NUMBER(19, 0) NOT NULL,
-	r_num           NUMBER(10, 0) NOT NULL,
+	id          VARCHAR2(255 CHAR) NOT NULL,
+	r_num       NUMBER(10, 0)      NOT NULL,
 	r_type          NUMBER(3, 0),
 	c_config        VARCHAR2(1000 CHAR),
-	f_data_view     NUMBER(19, 0),
+	f_data_view VARCHAR2(255 CHAR),
 	f_modifier_user NUMBER(19, 0),
 	c_title         VARCHAR2(255 CHAR),
 	PRIMARY KEY (id, r_num)
@@ -123,13 +134,13 @@ CREATE TABLE a_t_mts_report (
 ------------------------
 
 CREATE TABLE mt_mts_dataview_group (
-	f_data_view NUMBER(19, 0) NOT NULL,
-	f_group     NUMBER(19, 0) NOT NULL
+	f_data_view VARCHAR2(255 CHAR) NOT NULL,
+	f_group     VARCHAR2(255 CHAR) NOT NULL
 );
 
 CREATE TABLE mt_mts_report_group (
-	f_report NUMBER(19, 0) NOT NULL,
-	f_group  NUMBER(19, 0) NOT NULL
+	f_report VARCHAR2(255 CHAR) NOT NULL,
+	f_group  VARCHAR2(255 CHAR) NOT NULL
 );
 
 ----------------------
@@ -137,20 +148,21 @@ CREATE TABLE mt_mts_report_group (
 ----------------------
 
 CREATE TABLE t_mts_cfg_lob (
-	id              NUMBER(19, 0) NOT NULL,
-	d_creation      DATE          NOT NULL,
-	f_creator_user  NUMBER(19, 0),
+	id             VARCHAR2(255 CHAR) NOT NULL,
+	d_creation     DATE               NOT NULL,
+	f_creator_user NUMBER(19, 0)      NOT NULL,
 	d_modification  DATE,
 	f_modifier_user NUMBER(19, 0),
-	c_value         CLOB          NOT NULL,
-	n_version       NUMBER(10, 0) NOT NULL,
+	c_value        CLOB               NOT NULL,
+	n_version      NUMBER(10, 0)      NOT NULL,
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE t_mts_data_group (
-	id              NUMBER(19, 0)      NOT NULL,
+	id             VARCHAR2(255 CHAR) NOT NULL,
+	c_code         VARCHAR2(255 CHAR) NOT NULL,
 	d_creation      DATE               NOT NULL,
-	f_creator_user  NUMBER(19, 0),
+	f_creator_user NUMBER(19, 0)      NOT NULL,
 	d_modification  DATE,
 	f_modifier_user NUMBER(19, 0),
 	c_name          VARCHAR2(255 CHAR) NOT NULL,
@@ -159,11 +171,12 @@ CREATE TABLE t_mts_data_group (
 );
 
 CREATE TABLE t_mts_data_src (
-	id                       NUMBER(19, 0)      NOT NULL,
-	f_config                 NUMBER(19, 0),
-	f_connection             NUMBER(19, 0),
+	id               VARCHAR2(255 CHAR) NOT NULL,
+	f_config         VARCHAR2(255 CHAR) NOT NULL,
+	f_connection     NUMBER(19, 0)      NOT NULL,
+	e_conn_selection NUMBER(10, 0)      NOT NULL,
 	d_creation               DATE               NOT NULL,
-	f_creator_user           NUMBER(19, 0),
+	f_creator_user   NUMBER(19, 0)      NOT NULL,
 	c_key_field              VARCHAR2(255 CHAR),
 	d_modification           DATE,
 	f_modifier_user          NUMBER(19, 0),
@@ -176,27 +189,24 @@ CREATE TABLE t_mts_data_src (
 );
 
 CREATE TABLE t_mts_data_src_rel (
-	id                NUMBER(19, 0)      NOT NULL,
-	d_creation        DATE               NOT NULL,
-	f_creator_user    NUMBER(19, 0),
-	b_deleted         NUMBER(1, 0)       NOT NULL,
-	d_modification    DATE,
-	f_modifier_user   NUMBER(19, 0),
-	f_src_datasrc     NUMBER(19, 0),
-	c_src_ptr_field   VARCHAR2(255 CHAR) NOT NULL,
-	f_tgt_datasrc     NUMBER(19, 0),
-	c_tgt_key_field   VARCHAR2(255 CHAR),
-	c_tgt_title_field VARCHAR2(255 CHAR),
-	n_version         NUMBER(10, 0)      NOT NULL,
+	id              VARCHAR2(255 CHAR) NOT NULL,
+	d_creation      DATE               NOT NULL,
+	f_creator_user  NUMBER(19, 0)      NOT NULL,
+	d_modification  DATE,
+	f_modifier_user NUMBER(19, 0),
+	f_src_datasrc   VARCHAR2(255 CHAR) NOT NULL,
+	c_src_ptr_field VARCHAR2(255 CHAR) NOT NULL,
+	f_tgt_datasrc   VARCHAR2(255 CHAR) NOT NULL,
+	n_version       NUMBER(10, 0)      NOT NULL,
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE t_mts_data_view (
-	id              NUMBER(19, 0)      NOT NULL,
-	f_config        NUMBER(19, 0),
+	id             VARCHAR2(255 CHAR) NOT NULL,
+	f_config       VARCHAR2(255 CHAR) NOT NULL,
 	d_creation      DATE               NOT NULL,
-	f_creator_user  NUMBER(19, 0),
-	f_data_src      NUMBER(19, 0),
+	f_creator_user NUMBER(19, 0)      NOT NULL,
+	f_data_src     VARCHAR2(255 CHAR) NOT NULL,
 	d_modification  DATE,
 	f_modifier_user NUMBER(19, 0),
 	c_name          VARCHAR2(255 CHAR) NOT NULL,
@@ -206,29 +216,43 @@ CREATE TABLE t_mts_data_view (
 );
 
 CREATE TABLE t_mts_db_conn (
+	id               NUMBER(19, 0)      NOT NULL,
+	f_config         VARCHAR2(255 CHAR),
+	d_creation       DATE               NOT NULL,
+	f_creator_user   NUMBER(19, 0)      NOT NULL,
+	c_custom_param_1 VARCHAR2(255 CHAR),
+	c_driver         VARCHAR2(255 CHAR),
+	f_group          VARCHAR2(255 CHAR) NOT NULL,
+	d_modification   DATE,
+	f_modifier_user  NUMBER(19, 0),
+	c_name           VARCHAR2(255 CHAR) NOT NULL,
+	c_password       VARCHAR2(255 CHAR),
+	c_schema         VARCHAR2(255 CHAR),
+	c_test_query     VARCHAR2(255 CHAR),
+	c_url            VARCHAR2(255 CHAR),
+	c_username       VARCHAR2(255 CHAR) NOT NULL,
+	n_version        NUMBER(10, 0)      NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE t_mts_db_conn_alias (
 	id              NUMBER(19, 0)      NOT NULL,
-	f_config        NUMBER(19, 0),
+	f_connection   NUMBER(19, 0) NOT NULL,
 	d_creation      DATE               NOT NULL,
-	f_creator_user  NUMBER(19, 0),
-	c_driver        VARCHAR2(255 CHAR),
-	f_group         NUMBER(19, 0),
+	f_creator_user NUMBER(19, 0) NOT NULL,
+	e_mode         NUMBER(10, 0) NOT NULL,
 	d_modification  DATE,
 	f_modifier_user NUMBER(19, 0),
 	c_name          VARCHAR2(255 CHAR) NOT NULL,
-	c_password      VARCHAR2(255 CHAR),
-	c_schema        VARCHAR2(255 CHAR),
-	c_test_query    VARCHAR2(255 CHAR),
-	c_url           VARCHAR2(255 CHAR),
-	c_username      VARCHAR2(255 CHAR) NOT NULL,
 	n_version       NUMBER(10, 0)      NOT NULL,
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE t_mts_db_conn_grp (
-	id              NUMBER(19, 0)      NOT NULL,
-	f_config        NUMBER(19, 0),
+	id             VARCHAR2(255 CHAR) NOT NULL,
+	f_config       VARCHAR2(255 CHAR),
 	d_creation      DATE               NOT NULL,
-	f_creator_user  NUMBER(19, 0),
+	f_creator_user NUMBER(19, 0)      NOT NULL,
 	c_driver        VARCHAR2(255 CHAR),
 	d_modification  DATE,
 	f_modifier_user NUMBER(19, 0),
@@ -240,11 +264,11 @@ CREATE TABLE t_mts_db_conn_grp (
 );
 
 CREATE TABLE t_mts_report (
-	id              NUMBER(19, 0)      NOT NULL,
+	id             VARCHAR2(255 CHAR) NOT NULL,
 	c_config        VARCHAR2(1000 CHAR),
 	d_creation      DATE               NOT NULL,
-	f_creator_user  NUMBER(19, 0),
-	f_data_view     NUMBER(19, 0),
+	f_creator_user NUMBER(19, 0)      NOT NULL,
+	f_data_view    VARCHAR2(255 CHAR),
 	d_modification  DATE,
 	f_modifier_user NUMBER(19, 0),
 	c_title         VARCHAR2(255 CHAR) NOT NULL,
@@ -255,7 +279,7 @@ CREATE TABLE t_mts_report (
 CREATE TABLE t_mts_usr_prf (
 	id             NUMBER(19, 0) NOT NULL,
 	d_creation     DATE          NOT NULL,
-	f_dflt_conn    NUMBER(19, 0),
+	f_dflt_conn NUMBER(19, 0) NOT NULL,
 	d_modification DATE,
 	n_version      NUMBER(10, 0) NOT NULL,
 	PRIMARY KEY (id)
@@ -268,17 +292,23 @@ CREATE TABLE t_mts_usr_prf (
 ALTER TABLE t_mts_data_group
 ADD CONSTRAINT uk_mts_datagrp_name UNIQUE (c_name);
 
+ALTER TABLE t_mts_data_group
+ADD CONSTRAINT uk_mts_datagrp_code UNIQUE (c_code);
+
 ALTER TABLE t_mts_data_src
 ADD CONSTRAINT uk_mts_datasrc_name UNIQUE (c_name);
 
 ALTER TABLE t_mts_data_src_rel
-ADD CONSTRAINT datasrc_main UNIQUE (c_src_ptr_field, f_src_datasrc);
+ADD CONSTRAINT uk_datasrcrel_main UNIQUE (f_tgt_datasrc, f_src_datasrc, c_src_ptr_field);
 
 ALTER TABLE t_mts_data_view
 ADD CONSTRAINT uk_mts_dataview_name UNIQUE (c_name);
 
 ALTER TABLE t_mts_db_conn
-ADD CONSTRAINT uk_mts_dbconn_name UNIQUE (c_name);
+ADD CONSTRAINT uk_mts_dbConn_name UNIQUE (c_name);
+
+ALTER TABLE t_mts_db_conn_alias
+ADD CONSTRAINT uk_mts_dbConnAlias_all UNIQUE (c_name, e_mode);
 
 ALTER TABLE t_mts_db_conn_grp
 ADD CONSTRAINT uk_mts_dbconngrp_name UNIQUE (c_name);
@@ -288,12 +318,12 @@ ADD CONSTRAINT uk_mts_dbconngrp_name UNIQUE (c_name);
 ----------------------------------
 
 ALTER TABLE a_mt_mts_dataview_group
-ADD CONSTRAINT FK_as3vnbmsb0rsim7oi9hb7puw4
+ADD CONSTRAINT FK_2a3yxvoiots2j9blnf8pj8rq2
 FOREIGN KEY (r_num)
 REFERENCES REVINFO;
 
 ALTER TABLE a_mt_mts_report_group
-ADD CONSTRAINT FK_rqrfamcem36iwh2wg7hp9r7uv
+ADD CONSTRAINT FK_kj3byyseob19n68t1s93tg40y
 FOREIGN KEY (r_num)
 REFERENCES REVINFO;
 
@@ -324,6 +354,11 @@ REFERENCES REVINFO;
 
 ALTER TABLE a_t_mts_db_conn
 ADD CONSTRAINT FK_hx7nxcl41sklyncalbblegrmo
+FOREIGN KEY (r_num)
+REFERENCES REVINFO;
+
+ALTER TABLE a_t_mts_db_conn_alias
+ADD CONSTRAINT FK_3mccp5je6my7ydv8de6p3f5sh
 FOREIGN KEY (r_num)
 REFERENCES REVINFO;
 
@@ -459,6 +494,21 @@ ADD CONSTRAINT dbconn_mdfrusr2user
 FOREIGN KEY (f_modifier_user)
 REFERENCES t_dmt_user;
 
+ALTER TABLE t_mts_db_conn_alias
+ADD CONSTRAINT alias2connection
+FOREIGN KEY (f_connection)
+REFERENCES t_mts_db_conn;
+
+ALTER TABLE t_mts_db_conn_alias
+ADD CONSTRAINT dbconnalias_crtrusr2user
+FOREIGN KEY (f_creator_user)
+REFERENCES t_dmt_user;
+
+ALTER TABLE t_mts_db_conn_alias
+ADD CONSTRAINT dbconnalias_mdfrusr2user
+FOREIGN KEY (f_modifier_user)
+REFERENCES t_dmt_user;
+
 ALTER TABLE t_mts_db_conn_grp
 ADD CONSTRAINT dbconngrp2cfglob
 FOREIGN KEY (f_config)
@@ -503,18 +553,6 @@ REFERENCES t_dmt_user;
 -- CREATE SEQUENCES
 --------------------
 
-CREATE SEQUENCE mts_cfg_lob START WITH 1 INCREMENT BY 1;
-
-CREATE SEQUENCE mts_data_group START WITH 1 INCREMENT BY 1;
-
-CREATE SEQUENCE mts_data_src START WITH 1 INCREMENT BY 1;
-
-CREATE SEQUENCE mts_data_src_rel START WITH 1 INCREMENT BY 1;
-
-CREATE SEQUENCE mts_data_view START WITH 1 INCREMENT BY 1;
-
 CREATE SEQUENCE mts_db_conn START WITH 1 INCREMENT BY 1;
 
-CREATE SEQUENCE mts_db_conn_grp START WITH 1 INCREMENT BY 1;
-
-CREATE SEQUENCE mts_report START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE mts_db_conn_alias START WITH 1 INCREMENT BY 1;
